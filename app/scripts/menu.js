4508f16bc6092ba39f6b90f42a66ceaceec75417
Dick Kreisberg <rbkreisberg@gmail.com>
2013-09-09 01:40:56 -0700

    add menu and activate sliding side menu

--- app/scripts/menu.js
+++ app/scripts/menu.js
@@ -3,7 +3,16 @@ define([
 'use strict'
 
 function initializeMenu( deferred ){
+	setupSideMenu();
 	deferred.resolve();
+}
+
+function setupSideMenu() {
+	$('#sideMenuToggle').on('click', function () {
+	    $('#mainPanel').toggleClass('col-md-10 col-md-8').toggleClass('col-lg-10 col-lg-8');
+	    $('#sideMenu').toggleClass('col-md-2 col-md-4').toggleClass('col-lg-2 col-lg-4');
+	    $('#sideMenuButton').toggleClass('glyphicon-chevron-right glyphicon-chevron-left');
+	});
 }
 
 	var Menu = {
