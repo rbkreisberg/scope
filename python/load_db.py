ccffc0a5ef964ea3ad871a08df45663d90fe5c2f
Dick Kreisberg <rkreisberg@systemsbiology.org>
2013-09-10 13:51:37 -0700

    create indices after table is created and populated

--- python/load_db.py
+++ python/load_db.py
@@ -37,10 +37,23 @@ def denormalizeInput(inputFilename, tempFile):
 def connectToDatabase(host, port,  database, user, password):
 	return psycopg2.connect(host=host, port=port, database=database, user=user, password=password)
 
-def loadDatabase(loadFile, conn):
+def loadDatabase(loadFile,tableName, conn):
 	load = open(loadFile,'r')
 	cur = conn.cursor()
-	cur.copy_from(load, 'variant_tests_090913', sep='\t', columns=('chr','start','test','score'))
+	cur.copy_from(load, tableName, sep='\t', columns=('chr','start','test','score'))
+	cur.execute('CREATE TABLE IF NOT EXISTS summary_%s (id SERIAL, label VARCHAR(60),' +
+		' chr VARCHAR(3), start integer, stop integer, test VARCHAR(60), test_type VARCHAR(30),' +
+		' test_model VARCHAR(10), target VARCHAR(50), target_label VARCHAR(50), sample VARCHAR(10),' +
+		' score double precision, info JSON);',tableName)
+	cur.execute('INSERT INTO summary_%s (label, chr, start, test, test_type, test_model,' +
+		' target, target_label, sample, score) SELECT label, chr, start, test,' +
+		' split_part(split_part(test, '|', 1),':',1), split_part(split_part(test, '|', 1),':',2),' +
+		' split_part(test, '|', 2), split_part(split_part(test,'|',2),':',3), split_part(test,'|',3),' +
+		' score from variant_tests_090913')
+	cur.execute('create index on variant_tests_summary_090913 (score asc)')
+	cur.execute('create index on variant_tests_summary_090913 (target_label)')
+	cur.execute('create index on variant_tests_summary_090913 (test_type)')
+	cur.execute('create index on variant_tests_summary_090913 (test_type, test_model)')
 	conn.commit()
 	cur.close()
 
@@ -58,15 +71,16 @@ def parse_parameters():
     parser.add_argument('--password', nargs = '?', required=True,
                             help = 'database password')
     parser.add_argument('--port', nargs = '?', default=5433,
-                            help = 'database port')    
+                            help = 'database port')
+    parser.add_argument('--table', nargs = '?', required=True, help = 'database table name')
     return parser.parse_args()
 
 def main():
 	tempFile = '.tmp_db'
 	args = parse_parameters()
 	conn = connectToDatabase(args.host, args.port, args.db, args.user, args.password)
-	# denormalizeInput(args.input, tempFile)
-	loadDatabase(tempFile, conn)
+	denormalizeInput(args.input, tempFile)
+	loadDatabase(tempFile, args.table, conn)
 	conn.close()
 
 if __name__ == '__main__':
